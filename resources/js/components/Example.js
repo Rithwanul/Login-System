37f26c230a7033a912cd965f8ddf7fe228d88cc0
Rithwan <Rithwan@DESKTOP-N1C8I9M>
2020-11-15 17:52:00 +0600

    Adding Navbar

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Link,
+    Route
+} from "react-router-dom";
+
+import Nav from './Nav.js';
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Router>
+            <>
+                <Nav />
+                <Switch>
+
+                </Switch>
+            </>
+        </Router>
     );
 }
 
